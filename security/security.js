4fbf76401c149399e6363758eae14524b2412256
Red <redson1607@gmail.com>
2026-04-27 13:32:28 -0300

    Update security.js

--- security/security.js
+++ security/security.js
@@ -1,5 +1,5 @@
 /**
- * MÓDULO DE SEGURANÇA - CINEMAX
+ * MÓDULO DE SEGURANÇA - CINEDRAGON
  * Proteção contra invasões e vulnerabilidades comuns
  */
 
@@ -182,7 +182,7 @@ function addSecurityHeaders() {
   // Content Security Policy
   const cspMeta = document.createElement('meta');
   cspMeta.httpEquiv = 'Content-Security-Policy';
-  cspMeta.content = "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src https://fonts.gstatic.com";
+  cspMeta.content = "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src https://fonts.gstatic.com; img-src 'self' https://image.tmdb.org https://media.themoviedb.org data:;";
   document.head.appendChild(cspMeta);
   
   // X-UA-Compatible
